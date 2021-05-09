1ca440cbb7505e7c6c7444fd05810ac59fe0ebcb
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-05-09 21:06:04 +0200

    Added flask

---
+++ python_stack/flask_/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+@app.route('/success')
+def success():
+  return "success"
+
+@app.route("/hello/<name>")
+def hello(name):
+    print(name)
+    return "Hello "+ name
+
+@app.route("/user/<username>/<id>")
+def show_user_profile(username,id):
+    print(username)
+    print(id)
+    return "username:{} id:{}".format(username,id)
+
+if(__name__)=="__main__":
+    app.run(debug=True)
+
+
+
+
+    
