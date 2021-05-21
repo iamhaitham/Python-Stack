2e30e175265e61716875372edd0e6ae76b532352
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-05-21 22:01:32 +0200

    Added Dojos & Ninjas with Template Assignment

---
+++ python_stack/django/django_orm/dojo_ninjas_proj_with_template/dojo_ninjas_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Dojo(models.Model):
+    name=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=2)
+    desc=models.TextField(default="old dojo")
+    #ninjas=A list of ninjas in a certain dojo. This is called "Reverse Lookup with related_name".
+
+class Ninja(models.Model):
+    dojo=models.ForeignKey(Dojo,related_name="ninjas",on_delete=models.CASCADE)
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+
+def show_dojos():
+    return Dojo.objects.all()
+
+def add_dojo(dojo_name,dojo_city,dojo_state):
+    Dojo.objects.create(name=dojo_name,city=dojo_city,state=dojo_state,desc="old dojo")
+
+def add_ninja(ninja_select_dojo,ninja_first_name,ninja_last_name):
+    Ninja.objects.create(dojo=Dojo.objects.get(name=ninja_select_dojo),first_name=ninja_first_name,last_name=ninja_last_name)
---
+++ python_stack/django/django_orm/dojo_ninjas_proj_with_template/dojo_ninjas_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,HttpResponse,redirect
+from . import models
+
+def index(request):
+    context={
+    "All_Dojos":models.show_dojos(),
+    }
+    return render(request,"index.html",context)
+
+
+def Dojo(request):
+    if request.method=="POST":
+        dojo_name=request.POST["name"]
+        dojo_city=request.POST["city"]
+        dojo_state=request.POST["state"]
+        models.add_dojo(dojo_name,dojo_city,dojo_state)
+        return redirect("/")
+    else:
+        return HttpResponse("You are not allowed to change the route manually!")
+
+
+def Ninja(request):
+    if request.method=="POST":
+        ninja_first_name=request.POST["first_name"]
+        ninja_last_name=request.POST["last_name"]
+        ninja_select_dojo=request.POST["select_dojo"]
+        models.add_ninja(ninja_select_dojo,ninja_first_name,ninja_last_name)
+        return redirect("/")
+    else:
+        return HttpResponse("You are not allowed to change the route manually!")
